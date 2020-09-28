9a69a044dc755c1bc8343b9b24f395e84ad70ffe
Opilane TTHK <opilane@tthk.ee>
2020-09-28 14:50:35 +0300

    White and Array

---
+++ Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] shoppingList = { "milk", "bread", "WC paper", "cookies", "pork"};
+
+            int arrayLength = shoppingList.Length;
+            Console.WriteLine($"You have {arrayLength} on your list.");
+
+            for (int i = 0; i < shoppingList.Length; i++)
+            {
+                Console.WriteLine($"{i+1}. {shoppingList[i]}");
+            }
+            
+            Console.WriteLine($"The first item on your list is {shoppingList[0]}");
+            Console.WriteLine($"The last item on your list is {shoppingList[shoppingList.Length-1]}");
+        }
+    }
+}
---
+++ WhiteAndArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhiteAndArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int i = 1;
+            while(i != 11)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            int j = 9;
+            while (j != 0)
+            {
+                Console.WriteLine(j);
+                j--;
+            }
+
+
+        }
+    }
+}
